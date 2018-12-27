17ce7fefc9e0b660107e31f64318c0dde03c32fa
ziadoof <ziadoof@gmail.com>
2018-12-27 14:24:36 +0100

    fosuser install and configre

---
+++ src/Entity/User.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ziadoof
+ * Date: 27/12/18
+ * Time: 14:00
+ */
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=false)
+     * @Assert\NotBlank(message="Please enter your firstname.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=30,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     * )
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=false)
+     * @Assert\NotBlank(message="Please enter your lastname.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=30,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    private $lastname;
+}
