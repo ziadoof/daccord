5d8bd7f1abef0108d011d5fc3df2b909bd9ce314
ziadoof <ziadoof@gmail.com>
2018-12-27 14:58:33 +0100

    make crud user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ziadoof
- * Date: 27/12/18
- * Time: 14:00
- */
 
 namespace App\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -21,11 +15,111 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class User extends BaseUser
 {
     /**
-     * @ORM\Id
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $emailStatus;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $postalCode;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $phoneNumber;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $phonNumberStatus;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $birthday;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $birthdayStatus;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $mapX;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $mapY;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $driver;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     */
+    private $car;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $profileImage;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $carImage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $maxDistance;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $point;
 
     public function __construct()
     {
@@ -33,29 +127,248 @@ class User extends BaseUser
         // your own logic
     }
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    /**
-     * @ORM\Column(type="string", length=30, nullable=false)
-     * @Assert\NotBlank(message="Please enter your firstname.", groups={"Registration", "Profile"})
-     * @Assert\Length(
-     *     min=3,
-     *     max=30,
-     *     minMessage="The name is too short.",
-     *     maxMessage="The name is too long.",
-     * )
-     */
-    private $firstname;
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
 
-    /**
-     * @ORM\Column(type="string", length=30, nullable=false)
-     * @Assert\NotBlank(message="Please enter your lastname.", groups={"Registration", "Profile"})
-     * @Assert\Length(
-     *     min=3,
-     *     max=30,
-     *     minMessage="The name is too short.",
-     *     maxMessage="The name is too long.",
-     *     groups={"Registration", "Profile"}
-     * )
-     */
-    private $lastname;
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmailStatus(): ?bool
+    {
+        return $this->emailStatus;
+    }
+
+    public function setEmailStatus(bool $emailStatus): self
+    {
+        $this->emailStatus = $emailStatus;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?int
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(?int $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?int
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(?int $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getPhonNumberStatus(): ?bool
+    {
+        return $this->phonNumberStatus;
+    }
+
+    public function setPhonNumberStatus(bool $phonNumberStatus): self
+    {
+        $this->phonNumberStatus = $phonNumberStatus;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getBirthday(): ?\DateTimeInterface
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(?\DateTimeInterface $birthday): self
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    public function getBirthdayStatus(): ?bool
+    {
+        return $this->birthdayStatus;
+    }
+
+    public function setBirthdayStatus(bool $birthdayStatus): self
+    {
+        $this->birthdayStatus = $birthdayStatus;
+
+        return $this;
+    }
+
+    public function getMapX(): ?float
+    {
+        return $this->mapX;
+    }
+
+    public function setMapX(?float $mapX): self
+    {
+        $this->mapX = $mapX;
+
+        return $this;
+    }
+
+    public function getMapY(): ?float
+    {
+        return $this->mapY;
+    }
+
+    public function setMapY(?float $mapY): self
+    {
+        $this->mapY = $mapY;
+
+        return $this;
+    }
+
+    public function getDriver(): ?bool
+    {
+        return $this->driver;
+    }
+
+    public function setDriver(?bool $driver): self
+    {
+        $this->driver = $driver;
+
+        return $this;
+    }
+
+    public function getCar(): ?string
+    {
+        return $this->car;
+    }
+
+    public function setCar(?string $car): self
+    {
+        $this->car = $car;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getProfileImage(): ?string
+    {
+        return $this->profileImage;
+    }
+
+    public function setProfileImage(?string $profileImage): self
+    {
+        $this->profileImage = $profileImage;
+
+        return $this;
+    }
+
+    public function getCarImage(): ?string
+    {
+        return $this->carImage;
+    }
+
+    public function setCarImage(?string $carImage): self
+    {
+        $this->carImage = $carImage;
+
+        return $this;
+    }
+
+    public function getMaxDistance(): ?int
+    {
+        return $this->maxDistance;
+    }
+
+    public function setMaxDistance(int $maxDistance): self
+    {
+        $this->maxDistance = $maxDistance;
+
+        return $this;
+    }
+
+    public function getPoint(): ?int
+    {
+        return $this->point;
+    }
+
+    public function setPoint(int $point): self
+    {
+        $this->point = $point;
+
+        return $this;
+    }
 }
