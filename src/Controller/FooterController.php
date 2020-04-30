ca7bf1cd3e5df95c98bbb703fb6d25cfe9608462
ziadoof <ziadoof@gmail.com>
2020-04-30 23:16:20 +0200

    footer ok

---
+++ src/Controller/FooterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FooterController extends AbstractController
+{
+    /**
+     * @Route("/about-us", name="about_us")
+     */
+    public function aboutAs()
+    {
+        return $this->render('aboutus.html.twig');
+    }
+
+    /**
+     * @Route("/legal-notice", name="legal_notice")
+     * @param Request $request
+     * @return Response
+     */
+    public function Legal_notice(Request $request)
+    {
+        $locale = $request->getLocale();
+        $legal = 'legal_notice_'.$locale.'.html.twig';
+        return $this->render($legal);
+    }
+}
